b29ed41813eea7893feb9a54e3e4f15d6632d960
Andy Slocum <slocum@gmail.com>
2025-12-06 14:17:57 -0500

    working qt/vlc on mac

--- button/button2.py
+++ button/button2.py
@@ -3,21 +3,33 @@ from PyQt6.QtWidgets import (
     QApplication,
     QMainWindow,
     QFrame,
-    QVBoxLayout,
-    QHBoxLayout,
-    QPushButton,
-    QFileDialog
+    QVBoxLayout
 )
 from PyQt6.QtCore import QTimer, Qt
 import vlc
 
+import configparser
+import urllib.request
+
+kill_process = False
+requested_video_name = None
+idle_is_playing = True
+CONFIG_VIDEO_PATH = "video_folder"
+CONFIG_IDLE_VIDEO_NAME = "idle_video"
+CONFIG_SERVER_URL = "server_url"
+FPS_MAX = "fps_max"
+config = None
+
+def read_configuration():
+    config_ini = configparser.ConfigParser(allow_unnamed_section=True)
+    config_ini.read("config.ini")
+    return config_ini
+
+
 class VideoPlayer(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("PyQt6 VLC Media Player")
-        self.setGeometry(100, 100, 800, 600)
-
-        self.filename = ""
+        self.showFullScreen()
 
         # Create a VLC instance and a media player.
         self.instance = vlc.Instance()
@@ -28,24 +40,71 @@ class VideoPlayer(QMainWindow):
         self.setCentralWidget(self.widget)
         self.layout = QVBoxLayout()
         self.widget.setLayout(self.layout)
+        #self.widget.setWindowFlags(0x00000800)
+        self.setStyleSheet("background-color: black;")
 
         # Create the video frame where VLC will render the video.
         self.video_frame = QFrame()
-        self.video_frame.setStyleSheet("background: black;")
+        self.video_frame.setStyleSheet("background: black; border: 0px; padding: 0px; margin: 0px")
         self.layout.addWidget(self.video_frame)
 
-        # Create a horizontal layout for control buttons and progress slider.
-        self.controls_layout = QHBoxLayout()
-        self.layout.addLayout(self.controls_layout)
+        #timer to check events
+        self.timer = QTimer(self)
+        self.timer.setInterval(500)
+        self.timer.timeout.connect(self.updateUI)
+        self.timer.start()
 
-        # Open button: Open a video file.
-        self.open_button = QPushButton("Open Video")
-        self.open_button.clicked.connect(self.open_file)
-        self.controls_layout.addWidget(self.open_button)
+    def video_directory(self):
+        global config
+        global CONFIG_VIDEO_PATH
+        return f"{config[configparser.UNNAMED_SECTION][CONFIG_VIDEO_PATH]}"
 
-    def open_file(self):
+    def idle_video(self):
+        global config
+        global CONFIG_IDLE_VIDEO_NAME
+        return f"{self.video_directory()}/{config[configparser.UNNAMED_SECTION][CONFIG_IDLE_VIDEO_NAME]}"
+
+    def play_idle_video(self):
+        global idle_is_playing
+        if(self.mediaplayer.is_playing()):
+            return
+        idle_is_playing = True
+        self.open_file(self.idle_video())
+        return
+    
+    def updateUI(self):
+        global requested_video_name
+        global kill_process
+        self.play_idle_video()
+        requested_video_name = self.check_video_request()
+        if requested_video_name:
+            print(requested_video_name)
+            self.play_video(f"{self.video_directory()}/{requested_video_name}")
+            requested_video_name = None
+            return
+
+
+    def check_video_request(self):
+        global requested_video_name
+        global CONFIG_SERVER_URL
+        url = f"{config[configparser.UNNAMED_SECTION][CONFIG_SERVER_URL]}/watch"
+        contents = urllib.request.urlopen(url).read()
+        if contents:
+            #debug(f"scanned <{contents.decode('utf-8')}>")
+            return contents.decode('utf-8')
+        return None
+
+    def play_video(self, video):
+        global idle_is_playing
+        if (requested_video_name is not None) & (idle_is_playing == True):
+            self.stop_video()
+            idle_is_playing = False
+            self.open_file(video)
+
+
+    def open_file(self, video):
         # Open a file dialog to select a video file.
-        self.filename, _ = QFileDialog.getOpenFileName(self, "Open Video")
+        self.filename = video
         if self.filename:
             media = self.instance.media_new(self.filename)
             self.mediaplayer.set_media(media)
@@ -59,14 +118,11 @@ class VideoPlayer(QMainWindow):
                 self.mediaplayer.set_nsobject(int(self.video_frame.winId()))
             self.mediaplayer.play()
 
-    def pause_video(self):
-        # Pause or resume video playback.
-        self.mediaplayer.pause()
-
     def stop_video(self):
         self.mediaplayer.stop()
 
 if __name__ == "__main__":
+    config = read_configuration()
     app = QApplication(sys.argv)
     player = VideoPlayer()
     player.show()
